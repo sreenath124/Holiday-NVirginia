bd0bd4bcd831bcbee86fdceaba52359f482485bd
Sreenath P G <sreenathg@qburst.com>
2017-01-17 11:36:54 +0530

    form validation corrected

--- js/script.js
+++ js/script.js
@@ -9,21 +9,24 @@ $(document).ready(function() {
         event.preventDefault();
         validateForm();
     });
-    $('.form_section input[type="text"], footer input[type="text"]').on('keyup', function() {
+    $('.form_section, footer').find('input[type="text"]').on('keyup', function() {
         formReset();
+        $(this).prev('label').removeClass('required');
     });
     $('input[type="radio"]').on('change', function() {
         formReset();
     });
+    $('footer select').on('change', function() {
+        $(this).parent().siblings('label').removeClass('required');
+    });
 
     function validateForm() {
-        $('.form_section input[type="text"], footer input[type="text"] ').each(function(a, i) {
+        $('.form_section input[type="text"], footer input[type="text"] ').each(function() {
             if ($(this).val() === '') {
                 $(this).prev('label').addClass('required');
                 if ($('.errors').find(this.id).length === 0) {
                     $('.errors').append('<p class="error_msg" id=' + this.id + '>Please enter a valid ' + $(this).attr('name') + '.</p>');
                 }
-
             }
         });
 
@@ -34,25 +37,21 @@ $(document).ready(function() {
             }
         });
 
+        $('footer').find('select').each(function(){
+            if($(this).val()===null) {
+                $('.errors').append('<p class="error_msg"> Please select a ' + $(this).attr('name') + '.</p>');
+                $(this).parent().siblings('label').addClass('required');
+            }
+        });
+
         if (!$('.card_type input').is(":checked")) {
             $('.errors').append('<p class="error_msg">Please select a card type.</p>');
             $('.card_type label').addClass('required');
         }
-
-        if ($('#month').val() === null) {
-            $('.errors').append('<p class="error_msg">Please select a month.</p>');
-            $('#month').parent().siblings('label').removeClass('required').addClass('required');
-        }
-        if ($('#year').val() === null) {
-            $('.errors').append('<p class="error_msg">Please select an year.</p>');
-            $('#year').parent().siblings('label').removeClass('required').addClass('required');
-        }
-
     }
 
     function formReset() {
         $('.errors').empty();
-        $('form label').removeClass('required');
     }
 
 });
